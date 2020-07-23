d8797e8a340325fda243326cd43aaf1185b663ae
rory cawley <rory.cawley@gmail.com>
2020-07-23 14:47:02 +0100

    removed a test

--- tests/unit/pages/App.test.tsx
+++ tests/unit/pages/App.test.tsx
@@ -1,42 +1,36 @@
-import React from "react";
-import { render, screen, getByRole } from "@testing-library/react";
-import { Router, MemoryRouter } from "react-router-dom";
-import { createMemoryHistory } from "history";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react'
+import { render, screen, getByRole } from '@testing-library/react'
+import { Router, MemoryRouter } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+import '@testing-library/jest-dom/extend-expect'
 
-import App from "components/Root/App";
+import App from 'components/Root/App'
 
 // test utils file
 function renderWithRouter(
   ui: JSX.Element,
-  {
-    route: string = "/",
-    history = createMemoryHistory({ initialEntries: ["/"] }),
-  } = {}
+  { route: string = '/', history = createMemoryHistory({ initialEntries: ['/'] }) } = {}
 ) {
-  const Wrapper:
-    | React.ComponentClass<{}, any>
-    | React.FunctionComponent<{}>
-    | undefined = (props: { children?: React.ReactNode }) => (
-    <Router history={history}>{props.children}</Router>
-  );
+  const Wrapper: React.ComponentClass<{}, any> | React.FunctionComponent<{}> | undefined = (props: {
+    children?: React.ReactNode
+  }) => <Router history={history}>{props.children}</Router>
   return {
     ...render(ui, { wrapper: Wrapper }),
     // adding `history` to the returned utilities to allow us
     // to reference it in our tests (just try to avoid using
     // this to test implementation details).
     history,
-  };
+  }
 }
 
-describe("<App>", () => {
-  it("renders the App page", () => {
-    const history = createMemoryHistory();
+describe('<App>', () => {
+  it('renders the App page', () => {
+    const history = createMemoryHistory()
     const { container, getByText } = render(
       <Router history={history}>
         <App />
       </Router>
-    );
+    )
     // Arrange
     // const component = <Home />
 
@@ -44,23 +38,23 @@ describe("<App>", () => {
     // render(component)
 
     // Assert
-    expect(container.innerHTML).toMatch("Home");
-    expect(container.innerHTML).toMatch("Login");
+    expect(container.innerHTML).toMatch('Home')
+    expect(container.innerHTML).toMatch('Login')
 
     // expect(screen.getAllByText(/Home/)).toBeInTheDocument()
-  });
+  })
 
-  it.skip("landing on a bad page shows 404 page", () => {
-    const history = createMemoryHistory();
-    history.push("/some/bad/route");
-    const { container, getByRole } = render(
-      <Router history={history}>
-        <App />
-      </Router>
-    );
-    screen.debug();
-    expect(container.innerHTML).toHaveTextContent("404 Not Found");
-  });
+  // it.skip('landing on a bad page shows 404 page', () => {
+  //   const history = createMemoryHistory()
+  //   history.push('/some/bad/route')
+  //   const { container, getByRole } = render(
+  //     <Router history={history}>
+  //       <App />
+  //     </Router>
+  //   )
+  //   screen.debug()
+  //   expect(container.innerHTML).toHaveTextContent('404 Not Found')
+  // })
 
   // it('full app rendering/navigating', () => {
   //   const { container, getByText } = render(<App />, { wrapper: MemoryRouter })
@@ -68,10 +62,10 @@ describe("<App>", () => {
   //   expect(container.innerHTML).toHaveTextContent('Home')
   // })
 
-  it.skip("landing on a bad page", () => {
+  it.skip('landing on a bad page', () => {
     const { container } = renderWithRouter(<App />, {
-      route: "/something-that-does-not-match",
-    });
-    expect(container.innerHTML).toMatch("404 Not Found");
-  });
-});
+      route: '/something-that-does-not-match',
+    })
+    expect(container.innerHTML).toMatch('404 Not Found')
+  })
+})
