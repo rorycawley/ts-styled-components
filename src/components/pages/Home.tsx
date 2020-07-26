fe969e589837f869921277d417aeaef07d6b23bb
rory cawley <rory.cawley@gmail.com>
2020-07-26 09:51:06 +0100

    add lorem text

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,6 +3,17 @@ import React from 'react'
 import { PageLayout } from 'components/templates'
 
 const Home = () => {
-  return <PageLayout>Home</PageLayout>
+  return (
+    <PageLayout>
+      <h1>Home</h1>
+      <p>
+        <div className="gen300">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum accusantium nesciunt architecto saepe
+          perferendis recusandae iste, laboriosam obcaecati, dolorem quod mollitia voluptatum deleniti, tempore aliquam.
+          Libero explicabo illum optio aspernatur?
+        </div>
+      </p>
+    </PageLayout>
+  )
 }
 export default Home
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -3,6 +3,16 @@ import React from 'react'
 import { PageLayout } from 'components/templates'
 
 const Login = () => {
-  return <PageLayout>Login</PageLayout>
+  return (
+    <PageLayout>
+      <h1>Login</h1>
+      <p>
+        <div className="gen300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat nulla quo possimus mollitia iure tenetur?
+          Quisquam a pariatur alias aperiam, illo sint, debitis doloremque deserunt autem, molestiae at eum laudantium!
+        </div>
+      </p>
+    </PageLayout>
+  )
 }
 export default Login
