260c37db56a60e21515a2ebb495822a076800aef
rory cawley <rory.cawley@gmail.com>
2020-07-26 05:54:54 +0100

    correct active links

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React, { FC, ReactNode } from 'react'
 import styled from 'styled-components'
-import { Link, useLocation } from 'react-router-dom'
+import { Link as ReactRouterDomLink, useLocation } from 'react-router-dom'
 
 const HeaderWrapper = styled.header`
   height: 60px;
@@ -28,18 +28,23 @@ const Menu = styled.nav`
   top: initial;
 `
 
-interface StyledLinkProps {
+export interface LinkProps {
   isActive?: boolean
+  to: string
 }
-const StyledLink = styled.div`
-  a {
-    padding: 4px 8px;
-    display: block;
-    text-align: center;
-    box-sizing: border-box;
-    margin: auto 0;
-    font-weight: ${(props: StyledLinkProps) => (props.isActive ? 'bold' : 'normal')};
-  }
+export const Link: FC<LinkProps> = ({ isActive, to, children, ...props }) => (
+  <ReactRouterDomLink to={to} {...props}>
+    {children}
+  </ReactRouterDomLink>
+)
+
+const StyledLink = styled(Link)`
+  padding: 4px 8px;
+  display: block;
+  text-align: center;
+  box-sizing: border-box;
+  margin: auto 0;
+  font-weight: ${(props: LinkProps) => (props.isActive ? 'bold' : 'normal')};
 `
 
 const Header: FC = () => {
@@ -47,11 +52,11 @@ const Header: FC = () => {
   return (
     <HeaderWrapper>
       <Menu>
-        <StyledLink isActive={pathname === '/'}>
-          <Link to="/">Home</Link>
+        <StyledLink to="/" isActive={pathname === '/'}>
+          Home
         </StyledLink>
-        <StyledLink isActive={pathname === '/login'}>
-          <Link to="/login">Login</Link>
+        <StyledLink to="/login" isActive={pathname === '/login'}>
+          Login
         </StyledLink>
       </Menu>
     </HeaderWrapper>
